b891346a1a710eadf79cce4c85bd92f2ab4c8897
ugo <92054718+ndujesco@users.noreply.github.com>
2022-09-01 23:57:58 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const app = express();
+
+const projects = [
+  {
+    title: "E-Commerce Store",
+    description:
+      "A typical E-Commerce website for buying products. One gets to display their own products also.",
+    tech: "Nodejs | EJS | MongoDB",
+    github: "https://github.com/ndujesco/shop-website",
+    website: "https://ugo-shop.herokuapp.com/",
+  },
+  {
+    title: "All time favourite movies",
+    description:
+      "Here, you get movie recommendations by viewing user's all time favourite movies. You can also contribute by creating your own list!",
+    tech: "Nodejs | EJS | MongoDB",
+    github: "https://github.com/ndujesco/favourite-movies",
+    website: "https://all-time-favourite.herokuapp.com/",
+  },
+  {
+    title: "Blog Website",
+    description:
+      "A simple blog website where I put down random thoughts. Performs basic CRUD operations",
+    github: "https://github.com/ndujesco/blog",
+    website: "https://random-musings.herokuapp.com/",
+    tech: "Flask | Jinja | Postgres",
+  },
+];
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/works", (req, res) => {
+  res.render("projects", { projects });
+});
+
+app.listen(process.env.PORT || 3000);
---
+++ public/index.js
@@ -0,0 +1,86 @@
+const pages = ["home", "about", "work", "contact"];
+
+const isNotLaptop = window.matchMedia("(max-width: 768px)").matches;
+if (!isNotLaptop) {
+  document.querySelector("ul.menu-items").style.backgroundColor = "";
+}
+
+function clickCheck() {
+  const isChecked = document.getElementById("Checkbox").checked;
+  const content = document.getElementById("fullPage");
+  isChecked && content.classList.add("content");
+  !isChecked && content.classList.remove("content");
+
+  const splitClass = document.body.classList.value.split(" ");
+  let spreadParam = [];
+  splitClass.forEach((elem) => {
+    const splitElem = elem.split("-");
+    spreadParam.push(...splitElem);
+  });
+  const page = spreadParam.find((elem) => pages.includes(elem));
+  const pageColor =
+    document.getElementsByClassName(page)[0].style.backgroundColor;
+  const isNotLaptop = window.matchMedia("(max-width: 768px)").matches;
+
+  document.querySelector("ul.menu-items").style.backgroundColor = isNotLaptop
+    ? pageColor
+    : "";
+  !isChecked && setTimeout(removeBackground, 1000);
+}
+
+function unCheck() {
+  document.getElementById("Checkbox").checked = false;
+  const content = document.getElementById("fullPage");
+  content.classList.remove("content");
+  setTimeout(removeBackground, 1000);
+}
+
+function removeBackground() {
+  document.querySelector("ul.menu-items").style.backgroundColor = "";
+}
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Hi, I'm Ugochukwu", "Backend developer", "Undergraduate"];
+const typingDelay = 100;
+const erasingDelay = 80;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
